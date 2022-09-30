c217e549284a707e478c54dce749d9df6cdb2271
Ewen Domeon <41767402+FanaticalRoute@users.noreply.github.com>
2022-09-30 19:12:46 +0200

    Update code2.js

--- code2.js
+++ code2.js
@@ -2,7 +2,7 @@ function auth2 (){
     const pin = document.getElementById("pin").value;
 
     if (pin == "561") {
-        window.location.assign("https://google.com");
+        window.location.assign("https://fanaticalroute.github.io/enigme/zfinish4503.html");
         alert("Bien joué");
     } else {
         alert("Try Again...");
